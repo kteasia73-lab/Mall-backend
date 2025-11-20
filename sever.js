6e8409b15c80eda3b6f0425c7bb319ef1fb1fa58
kteasia73-lab <kteasia73@gmail.com>
2025-11-21 06:13:27 +1300

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import adminRoutes from "./src/routes/adminRoutes.js";
+import merchantRoutes from "./src/routes/merchantRoutes.js";
+import mallRoutes from "./src/routes/mallRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+// Routes
+app.use("/api/admin", adminRoutes);
+app.use("/api/merchant", merchantRoutes);
+app.use("/api/mall", mallRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Mall Backend Running Successfully!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

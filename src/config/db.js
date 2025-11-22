4f6c4675967398438e7af577f788e46ee4bb95d8
kteasia73-lab <kteasia73@gmail.com>
2025-11-23 12:50:45 +1300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Database connection failed", error.message);
+    console.log("Database connection failed", error.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+

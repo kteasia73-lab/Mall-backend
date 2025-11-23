28eaa7e2b870868f2425867b0c0fd6d811cf446b
kteasia73-lab <kteasia73@gmail.com>
2025-11-23 14:34:11 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
+
 import adminRoutes from "./src/routes/adminRoutes.js";
+import merchantRoutes from "./src/routes/merchantRoutes.js";
 
 dotenv.config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +18,7 @@ connectDB();
 
 // Routes
 app.use("/api/admin", adminRoutes);
+app.use("/api/merchant", merchantRoutes);
 
 app.get("/", (req, res) => {
   res.send("Mall Backend Running Successfully!");

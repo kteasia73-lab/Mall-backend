8295ecff1a94a0d2fcd34dc4672e1bd32607e390
kteasia73-lab <kteasia73@gmail.com>
2025-12-04 11:44:05 +1300

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { loginAdmin, createAdmin } from "../controllers/adminController.js";
+import { loginAdmin } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 // Admin login
 router.post("/login", loginAdmin);
-
-// TEMPORARY: Create admin account
-router.post("/create", createAdmin);
 
 export default router;
